6a446d0a6e1fb989f40bb82468a3eead57034668
Addy Fogle <134630607+addyfogle@users.noreply.github.com>
2026-01-26 07:57:52 -0700

    fix saving to db

--- src/main/java/com/example/demo/entities/Employee.java
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -1,10 +1,6 @@
 package com.example.demo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
@@ -12,19 +8,20 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Entity(name = "employees")
-@Table(name = "employees")
 @Component
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(sequenceName = "employees_employee_id_seq",
+    allocationSize = 1)
     private Long employee_id;
 
     private String first_name;
 
     private String last_name;
 
-            private String department;
+    private String department;
 
     private double salary;
 }
